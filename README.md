e85ad45900561aa02c61f65438dbeddd47e8baac
ccc <chenliang2380@cvte.com>
2017-05-20 17:03:54 +0800

    Add the inspectionlib and example

---
+++ README.md
@@ -0,0 +1,9 @@
+Introduction
+--------------------
+You can run the example as follow in cmd:
+"exp\inspectiontest.exe test2.jpg"
+or double click inspectiontest.exe 
+
+Question
+-----------------
+Any other question. please contact *chenliang*.
---
+++ inspectionlib/include/toolsinspection.h
@@ -0,0 +1,40 @@
+#ifndef TOOLSINSPECTION_H
+#define TOOLSINSPECTION_H
+
+#include "toolsinspection_global.h"
+#include <vector>
+#include "opencv2/core/core.hpp"
+
+using namespace cv;
+class InspectionManage;
+
+enum TOOL_RES
+{
+    TOOL_EXIST_ = 1,
+    TOOL_NOT_EXIS_ = 2,
+    TOOL_UNKNOWN_ = 3,
+    ERR_NO_TEMPLATE_ = 4,
+    ERR_INSPECT_IMG_ = 5
+};
+
+class TOOLSINSPECTIONLIBSHARED_EXPORT ToolsInspection
+{
+public:
+	ToolsInspection();
+	~ToolsInspection();
+    void setDebugInfo(bool debugState);
+
+    bool setTmplImageAndMarker(Mat tmplImage,
+                               std::vector<Rect> markerList = std::vector<Rect>());
+
+    bool setCurrentInspectImage(Mat inspectImage);
+
+    int inspection(Rect toolRect, std::vector<Point2f> &resRect,float &resScore, Mat toolImage=Mat());
+
+private:
+    InspectionManage * _pManage;
+
+};
+
+#endif //TOOLSINSPECTION_H
+
